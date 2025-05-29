03502002992cd509f4fc766a97624d0170884bf3
clara <cllara.marts@gmail.com>
2025-05-29 19:15:06 -0300

    adicionando testemonials

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const navbar = document.querySelector('.nav-links');
+
 const btn_transf = document.querySelector('.transf');
 const btn_loan = document.querySelector('.loan');
 const btn_close = document.querySelector('.close');
@@ -7,7 +9,27 @@ const div_loan = document.querySelector('.operation-div-2');
 const div_close = document.querySelector('.operation-div-3');
 
 
+//nav animation
+const navOpacityAnimation = function (e, opacity) {
+    if (e.target.classList.contains('nav-link')) {
+        const selected = e.target;
+        const links = selected.closest('.nav-links').querySelectorAll('.nav-link');
 
+        links.forEach(element => {
+            if (element !== selected) {
+                element.style.opacity = opacity;
+            }
+        });
+    }
+};
+
+navbar.addEventListener('mouseover', function (e) {
+    navOpacityAnimation(e, 0.3);
+});
+
+navbar.addEventListener('mouseout', function (e) {
+    navOpacityAnimation(e, 1);
+});
 
 
 const addActiveBtn = function (selected, btn2, btn3) {
